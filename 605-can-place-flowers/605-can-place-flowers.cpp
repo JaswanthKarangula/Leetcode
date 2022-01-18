1bd03965b904bc43d6f0263b8fd247a1a68ed674
Jaswanth Karangula <49749604+JaswanthKarangula@users.noreply.github.com>
2022-01-18 21:05:03 +0530

    Time: 393 ms (5.42%), Space: 20.2 MB (69.55%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& f, int c) {
+        int n=f.size();
+        int prev=0,next=0,count=0;
+        int curr=f[0];
+        for(int i=0;i<n;++i){
+            
+            if(i+1<n)next=f[i+1];
+            else next=0;
+            cout<<prev<<"  "<<curr<<"   "<<next<<endl;
+            if(prev==0 && next==0 && curr==0){
+                count++;
+                prev=1;
+                curr=next;
+            }
+            else{
+                prev=curr;
+                curr=next;
+            }
+            //cout<<prev<<"  "<<curr<<"   "<<next<<endl;
+        }
+        return count>=c;
+    }
+};
