5e0ff3235b4f55f492c43b782f2c44c76a6cf700
Jaswanth Karangula <49749604+JaswanthKarangula@users.noreply.github.com>
2022-05-02 21:39:55 +0530

    Time: 662 ms (30.85%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -2,3 +2,10 @@
 select employee_id, 
 if(employee_id%2 != 0 and name NOT LIKE 'M%', salary, 0) as bonus
 from employees order by employee_id; 
+
+# select employee_id, 
+# case 
+#     when employee_id%2 != 0 and name NOT LIKE 'M%' then salary
+#     else 0
+# end as bonus
+# from employees order by employee_id;
