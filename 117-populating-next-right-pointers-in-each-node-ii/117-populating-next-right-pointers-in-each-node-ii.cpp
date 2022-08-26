582fd7c1ae50f8be9eaa17b77ba0329b7fc6ab0f
Jaswanth Karangula <49749604+JaswanthKarangula@users.noreply.github.com>
2022-08-26 16:35:23 -0700

    Time: 22 ms (53.05%), Space: 17.3 MB (83.95%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -20,19 +20,54 @@ class Solution {
 public:
     Node* connect(Node* root) {
         if(root==nullptr)return root;
-        queue<Node*> q;
-        q.push(root);
-        while(!q.empty()){
-int siz=q.size();
-            Node* prev=NULL;
-            for(int i=0;i<siz;++i){
-                 auto curr=q.front();
-                q.pop();
-                if(prev!=NULL)prev->next=curr;
-                if(curr->left!=nullptr)q.push(curr->left);
-                if(curr->right!=nullptr)q.push(curr->right);
-                prev=curr;
+        
+        
+       //  With Extre Space O(N);
+//         queue<Node*> q;
+//         q.push(root);
+//         while(!q.empty()){
+// int siz=q.size();
+//             Node* prev=NULL;
+//             for(int i=0;i<siz;++i){
+//                  auto curr=q.front();
+//                 q.pop();
+//                 if(prev!=NULL)prev->next=curr;
+//                 if(curr->left!=nullptr)q.push(curr->left);
+//                 if(curr->right!=nullptr)q.push(curr->right);
+//                 prev=curr;
+//             }
+//         }
+//         return root;
+        
+        
+        // without extra space;
+        
+        Node* currParent =root;
+        Node* baseChild, *currChild, *nextChild;
+        
+        while(currParent){
+            
+            //move the parent to the first location which has a child
+            
+            while(currParent->next && !currParent->left && !currParent->right)currParent= currParent->next;
+            // mark the base child;
+            currChild = baseChild =  currParent->left ?currParent->left:currParent->right;
+            while(currChild){
+                if( currParent->right &&  (currChild!= currParent->right) ){
+                    nextChild = currParent->right;
+                }
+                else{
+                    //move the parent to the first location which has a child
+                    currParent = currParent->next;
+                    while (currParent && !currParent->left && !currParent->right) currParent = currParent->next; 
+                    nextChild = currParent ? currParent->left?currParent->left:currParent->right : currParent;
+                }
+                currChild->next = nextChild;
+                currChild = nextChild;
             }
+            currParent = baseChild;
+            
+            
         }
         return root;
         
