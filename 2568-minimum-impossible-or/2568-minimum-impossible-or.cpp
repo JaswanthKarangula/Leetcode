d692ca8717a8fbdd96ebed433685daa527082262
Jaswanth Karangula <49749604+JaswanthKarangula@users.noreply.github.com>
2023-03-13 20:08:13 -0700

    Time: 216 ms (63.52%), Space: 68.2 MB (61.47%) - LeetHub

--- 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
+++ 2568-minimum-impossible-or/2568-minimum-impossible-or.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int minImpossibleOR(vector<int>& nums) {
         int n = nums.size();
-        set<int> s;
-        for(int i=0;i<n;++i){
-            s.insert(nums[i]);
-        }
+        unordered_set<int> s(nums.begin(),nums.end());
         int p =1;
         while(s.find(p)!=s.end()){
             p=p*2;
