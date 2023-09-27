c13f31e686079ccb2848a199efe81bf1d6b9fcdb
Jaswanth Karangula <49749604+JaswanthKarangula@users.noreply.github.com>
2023-09-26 18:11:58 -0700

    Time: 89 ms (35.19%), Space: 59.9 MB (20.79%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& arr) {
+    int peakIndexInMountainArray(vector<int>& A) {
         
-        // given tow indices we can find of the first and last are we can say if the peak is in between or outside...
+        // we could easly split the search space into half  basically by comparing the mid and next to mid elemnt we could find if the peak is in the right side or left side  of the mid;
         //  [0,1,2,3,4,5,6,7,8,10,6,5,4,3,2] 
-        int n = arr.size();
-        int low =0;
-        int high =n-1;
-        int ans =-1;
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(arr[mid]<arr[mid+1]){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-                ans = mid;
-            }
+        int l = 0, r = A.size() - 1, mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (A[mid] < A[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
         }
-        return ans;
+        return l;
     }
 };
